f989bf909a74bfeec65ec57fb78be90718c92486
Jack <jfoltzt@gmail.com>
2017-07-24 10:33:21 -0700

    Update k5 library extension

--- do.sh
+++ do.sh
@@ -1,3 +1,3 @@
 g++ -c modelframe.cpp -o modelframe.o -I/usr/include/freetype2 -I/usr/include
 g++ -c main.cpp -o main.o -I/usr/include/freetype2 -I/usr/include
-g++ main.o modelframe.o -o thing.out -lGL -lfreetype -l:k5.a -lGLEW -lglfw -lassimp -lSOIL
+g++ main.o modelframe.o -o thing.out -lGL -lfreetype -lk5 -lGLEW -lglfw -lassimp -lSOIL
